e2e78d7e0015560d8da280e8011b64098f4ba5a7
Gianou <david.gianadda@students.hevs.ch>
2024-08-25 17:15:22 +0200

    Smaller find-prompt on mobile

--- src/app/components/overlay/find-prompt/find-prompt.component.ts
+++ src/app/components/overlay/find-prompt/find-prompt.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { GameService } from '../../../services/game.service';
+import { DeviceService } from '../../../services/device.service';
 
 @Component({
   selector: 'app-find-prompt',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './find-prompt.component.html',
   styleUrl: './find-prompt.component.css'
 })
 export class FindPromptComponent {
-  constructor(public gameService: GameService) {
+  constructor(
+    public gameService: GameService,
+    public deviceService: DeviceService
+  ) {
   }
 
 }
